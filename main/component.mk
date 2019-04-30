384ba94ccc9c701ef99ea4dfc45053341772daf9
Jiameng Shi <jiameng.shih@gmail.com>
2019-04-30 17:26:23 +0900

    fix: link error when mbedtls is enabled

--- main/component.mk
+++ main/component.mk
@@ -2,4 +2,7 @@
 # Main Makefile. This is basically the same as a component makefile.
 #
 COMPONENT_EMBED_FILES := assets/style.css assets/jquery.gz assets/code.js assets/index.html
-COMPONENT_EMBED_FILES += certs/cacert.pem certs/cert.pem certs/privkey.pem
+
+ifdef CONFIG_ENABLE_SECURITY_PROTO
+COMPONENT_EMBED_TXTFILES += certs/cacert.pem certs/cert.pem certs/privkey.pem
+endif
